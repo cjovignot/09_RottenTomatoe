cc37f1ac1fbddf778488fb3b050d502ddd002166
seb <sebastien.garcia@epitech.eu>
2023-05-11 19:23:10 +0200

    added movieid param in movieunit

--- boostedpotatoes/src/app/movieunit/[id]/page.js
+++ boostedpotatoes/src/app/movieunit/[id]/page.js
@@ -1,12 +1,13 @@
 import Image from 'next/image'
-import Film from '../components/film-info'
+import Film from '../../components/film-info'
 import React from 'react'
 
-export default function Home() {
+export default function Home({params}) {
+  console.log(params.id);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-       <Film/>
+       <Film id={params.id}/>
       </div>  
     </main>
   )
