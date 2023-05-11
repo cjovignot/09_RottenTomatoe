146d84874c0e92d4e0da48e08be2a4f1ff08473b
arthur <arthur.asselin@epitech.eu>
2023-05-11 10:08:33 +0200

    [ADD] index V2

--- boostedpotatoes/src/app/components/movie_card.js
+++ boostedpotatoes/src/app/components/movie_card.js
@@ -3,16 +3,13 @@ const Card = ({ movie }) => {
   const { title, poster_path, release_date, genres } = movie;
   return (
     <>
-      <div className="card w-96 bg-base-100 shadow-xl m-5">
+      <div className="card w-96 bg-base-100 shadow-xl m-5 h-110 hover:scale-105 transition duration-500">
         <figure>
           <img src={poster_path} alt={title} />
         </figure>
-        <div className="card-body ">
-          <h2 className="card-title">
-            {title}
-            <div className="badge badge-secondary">{release_date}</div>
-          </h2>
-          <div className="card-actions justify-end">
+        <div className="card-body text-center h-40">
+          <h2 className="card-title self-center">{title}</h2>
+          <div className="card-actions justify-between">
             {genres.map((genre) => (
               <div key={genre} className="badge badge-outline">
                 {genre}
--- boostedpotatoes/src/app/components/movie_list.js
+++ boostedpotatoes/src/app/components/movie_list.js
@@ -1,41 +1,82 @@
-// "use client";
+"use client";
 
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./movie_card";
 import Link from "next/link";
 import axios from "axios";
 
-const fetchData = async () => {
-  console.log("MovieList component je vais dans mon use_effect");
+const MovieList = () => {
+  const [pageNbr, setPageNbr] = useState(1);
+  const [movies, setMovies] = useState(null);
 
-  try {
-    console.log("Je vais dans mon try");
-    const response = await fetch("http://localhost:3002/movies/1"); // cache: 'force-cache' is the default("");
-    const movies = await response.json();
-    console.log("MON FETCH", movies);
-    return movies.movies;
-  } catch (error) {
-    console.error("fetch error", error);
-  }
-};
+  const fetchData = async (pageNbr) => {
+    console.log("Fetching movies for page", pageNbr);
 
-const MovieList = async () => {
-  const movies = await fetchData();
+    try {
+      const response = await axios.get(
+        "http://localhost:3002/movies/" + pageNbr,
+        {
+          cache: "force-cache",
+        }
+      );
+      const data = await response.data;
+      console.log("Fetched movies:", data);
+      setMovies(data);
+    } catch (error) {
+      console.error("Fetch error", error);
+    }
+  };
 
-  console.log("my movies", movies);
+  const nextPage = () => {
+    if (pageNbr < movies.totalPages) {
+      setPageNbr(pageNbr + 1);
+    }
+  };
+
+  const previousPage = () => {
+    if (pageNbr > 1) {
+      setPageNbr(pageNbr - 1);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(pageNbr);
+  }, [pageNbr]);
 
   return (
-    <div className="movie-list">
-      {movies ? (
-        movies.map((movie) => (
-          <Link key={movie._id} href={`/unit_view/${movie._id}`}>
-            <Card movie={movie} />
-          </Link>
-        ))
-      ) : (
-        <p>fuck</p>
-      )}
-    </div>
+    <>
+      <div className="movie-list">
+        {movies?.movies ? (
+          movies.movies.map((movie) => (
+            <Link key={movie._id} href={`/unit_view/${movie._id}`}>
+              <Card movie={movie} />
+            </Link>
+          ))
+        ) : (
+          <button className="btn loading text-center">loading</button>
+        )}
+      </div>
+      <div className="paginationindex">
+        <button
+          className="btn m-2"
+          onClick={previousPage}
+          disabled={pageNbr === 1}
+        >
+          Prev
+        </button>
+        <div className="bg-neutral-focus text-neutral-content rounded-full w-24">
+          <span className="text-3xl h-10">{movies?.currentPage}</span>
+        </div>
+
+        <button
+          className="btn m-2"
+          onClick={nextPage}
+          disabled={pageNbr === movies?.totalPages}
+        >
+          Next
+        </button>
+      </div>
+    </>
   );
 };
 
