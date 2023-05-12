4884683c57410c923b04645dea8ccbf693fa4d72
Cosme Jovignot <cosme.jovignot@epitech.eu>
2023-05-12 20:46:21 +0200

    [ADD] NPM in Launcher

--- boostedpotatoes/launcher.sh
+++ boostedpotatoes/launcher.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 cd src/app/api/
+npm install
 set -o allexport
-# source .env
 set +o allexport
 gnome-terminal --title="Users" -- node-dev users.js
 
 set -o allexport
-# source .env
 set +o allexport
 gnome-terminal --title="Movies" -- node-dev movies.js
 
 set -o allexport
-# source .env
 set +o allexport
 gnome-terminal --title="Auth" -- node-dev auth.js
 
-
+npm install
 npm run dev
