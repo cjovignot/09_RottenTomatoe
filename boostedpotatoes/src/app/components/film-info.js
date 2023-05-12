da88d27d8db4b81e95ff5cbc102d0b4ef13561f8
Cosme Jovignot <cosme.jovignot@epitech.eu>
2023-05-12 15:39:42 +0200

    [FIX] Hydration issues

--- boostedpotatoes/src/app/components/film-info.js
+++ boostedpotatoes/src/app/components/film-info.js
@@ -29,10 +29,10 @@ export default async function Film({ id }) {
           <div className="flex ">
             <div className="w-auto mx-2">
               <h1 className="text-center text-2xl font-bold">Rating</h1>
-              <div class="flex">
+              <div className="flex">
                 <svg
                   aria-hidden="true"
-                  class="w-5 h-5 text-yellow-400"
+                  className="w-5 h-5 text-yellow-400"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
@@ -40,10 +40,10 @@ export default async function Film({ id }) {
                   <title>Rating star</title>
                   <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
                 </svg>
-                <p class="ml-2 mr-2 text-sm font-bold text-gray-900 dark:text-white">
+                <p className="ml-2 mr-2 text-sm font-bold text-gray-900 dark:text-white">
                   {details.vote_average}
                 </p>
-                <p class="text-sm mr-2 w-max font-medium text-gray-900 dark:text-white">
+                <p className="text-sm mr-2 w-max font-medium text-gray-900 dark:text-white">
                   {details.vote_count} reviews
                 </p>
               </div>
