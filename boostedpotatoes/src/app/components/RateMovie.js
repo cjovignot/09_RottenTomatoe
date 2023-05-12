1a925ded919060526641e2beace350e43ccf7a9f
seb <sebastien.garcia@epitech.eu>
2023-05-12 10:39:37 +0200

    added rate debut

--- boostedpotatoes/src/app/components/RateMovie.js
+++ boostedpotatoes/src/app/components/RateMovie.js
@@ -5,16 +5,19 @@ import Cookies from "js-cookie";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-function rateMovie() {
+export default async function RateMovie() {
 
 
     return (
-        <div className="rating rating-md">
-            <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
-            <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" checked />
-            <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
-            <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
-            <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+        <div>
+            <div className="rating rating-md">
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+            </div>
+            <button className="btn btn-secondary">Button</button>
         </div>
     );
 };
